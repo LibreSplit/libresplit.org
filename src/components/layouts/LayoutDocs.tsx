ba4068402e7598a6e1e123186d543129eb0b1688
JackTench <79285604+JackTench@users.noreply.github.com>
2026-09-14 17:03:08 +0100

    Move button into layout
    Also switches to using A for navigation instead of navigate()

--- src/components/layouts/LayoutDocs.tsx
+++ src/components/layouts/LayoutDocs.tsx
@@ -1,11 +1,16 @@
+import { A } from "@solidjs/router";
+import { ArrowLeft } from "lucide-solid";
 import type { ParentProps } from "solid-js";
 
-import { AppDocsBackButton } from "../libresplit/AppDocsBackButton";
+import { Button } from "../ui/button";
 
 export function LayoutDocs(props: ParentProps) {
   return (
     <div>
-      <AppDocsBackButton />
+      <Button variant="outline" as={A} href="/docs">
+        <ArrowLeft />
+        <p>Docs</p>
+      </Button>
       {props.children}
     </div>
   );
