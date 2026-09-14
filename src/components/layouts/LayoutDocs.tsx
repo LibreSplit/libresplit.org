1678d170bc6ff045199a654fdb9b21470d5b7351
JackTench <79285604+JackTench@users.noreply.github.com>
2026-09-14 17:13:22 +0100

    Switch fully to using A component
    Linter seems to prefer an A that looks like a button, as opposed to a
    button that works like an A.

--- src/components/layouts/LayoutDocs.tsx
+++ src/components/layouts/LayoutDocs.tsx
@@ -2,22 +2,23 @@ import { A } from "@solidjs/router";
 import { ArrowLeft } from "lucide-solid";
 import type { ParentProps } from "solid-js";
 
-import { Button } from "../ui/button";
+import { buttonVariants } from "../ui/button";
 
 export function LayoutDocs(props: ParentProps) {
   return (
     <div>
       <nav class="mb-6">
-        <Button
-          variant="ghost"
-          size="sm"
-          class="-ml-2 text-muted-foreground hover:text-foreground"
-          as={A}
+        <A
           href="/docs"
+          class={buttonVariants({
+            variant: "ghost",
+            size: "sm",
+            class: "-ml-2 text-muted-foreground hover:text-foreground",
+          })}
         >
           <ArrowLeft />
           <p>Docs</p>
-        </Button>
+        </A>
       </nav>
 
       {props.children}
